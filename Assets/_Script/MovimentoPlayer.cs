ac5f693c3b713d13eed1f6b7f1e9a09d5a842585
MarceloCMiguel <marcelocmiguel@outlook.com>
2022-03-04 15:54:55 -0300

    movimento

--- Assets/_Script/MovimentoPlayer.cs
+++ Assets/_Script/MovimentoPlayer.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class MovimentoPlayer : MonoBehaviour
 {
 
+    Rigidbody2D m_Rigidbody;
     [Range(1,15)]
-    public float velocidade = 5.0f;
+    float m_Speed;
+    float horizontal_speed;
 
     // Start is called before the first frame update
     void Start()
     {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
 
+        m_Speed = 2.0f;
+
+        horizontal_speed = 300.0f;
         
         
     }
@@ -19,11 +25,48 @@ public class MovimentoPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float inputX = Input.GetAxis("Vertical");
+        // float inputY = Input.GetAxis("Vertical");
 
-        transform.Rotate(0.0f,0.0f,  -Input.GetAxis ("Horizontal") * velocidade);
+        // transform.Rotate(0.0f,0.0f,  -Input.GetAxis ("Horizontal") * velocidade);
 
-        transform.position += new Vector3(0, inputX, 0) * Time.deltaTime * velocidade;;
+        // transform.right = new Vector3(inputY,0,0);
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            //rotate the sprite about the Z axis in the positive direction
+            transform.Rotate(new Vector3(0, 0, -1) * horizontal_speed * Time.deltaTime, Space.World);
+
+            //Move the Rigidbody to the right constantly at speed you define (the red arrow axis in Scene view)
+            // m_Rigidbody.velocity = transform.right * m_Speed;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //rotate the sprite about the Z axis in the positive direction
+            transform.Rotate(new Vector3(0, 0, 1) * horizontal_speed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+            m_Rigidbody.AddForce(transform.right * 1.0f);
+
+
+            //Move the Rigidbody to the right constantly at speed you define (the red arrow axis in Scene view)
+            // m_Rigidbody.velocity = transform.right * m_Speed;
+        }
+
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //Move the Rigidbody to the left constantly at the speed you define (the red arrow axis in Scene view)
+            m_Rigidbody.velocity = -transform.right * m_Speed;
+        }
+        // else{
+        //     m_Rigidbody.velocity = transform.right * 0;
+        // }
+
+
+
         
     }
 }
