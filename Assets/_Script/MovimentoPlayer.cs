024bb2ffbc43a285ae571c19c5f41206153480be
MarceloCMiguel <marcelocmiguel@outlook.com>
2022-03-06 14:39:01 -0300

    two player

--- Assets/_Script/MovimentoPlayer.cs
+++ Assets/_Script/MovimentoPlayer.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class MovimentoPlayer : MonoBehaviour
 {
 
+    [Range(1,2)]
+    public int number_player;
+
     Rigidbody2D m_Rigidbody;
     [Range(1,15)]
     float m_Speed;
@@ -32,43 +35,60 @@ public class MovimentoPlayer : MonoBehaviour
     void Update()
     {
         is_inside = PointInsideSphere(transform.position, montanha.transform.position, montanha_radius);
-        Debug.Log(is_inside);
 
+        if(number_player ==1){
+            Debug.Log(m_Rigidbody.velocity);
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
+                //rotate the sprite about the Z axis in the positive direction
+                transform.Rotate(new Vector3(0, 0, -1) * horizontal_speed * Time.deltaTime, Space.World);
+            }
 
-            //rotate the sprite about the Z axis in the positive direction
-            transform.Rotate(new Vector3(0, 0, -1) * horizontal_speed * Time.deltaTime, Space.World);
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                //rotate the sprite about the Z axis in the positive direction
+                transform.Rotate(new Vector3(0, 0, 1) * horizontal_speed * Time.deltaTime, Space.World);
+            }
 
-            //Move the Rigidbody to the right constantly at speed you define (the red arrow axis in Scene view)
-            // m_Rigidbody.velocity = transform.right * m_Speed;
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                m_Rigidbody.AddForce(transform.right * 1.0f);
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                //Move the Rigidbody to the left constantly at the speed you define (the red arrow axis in Scene view)
+                m_Rigidbody.velocity = -transform.right * m_Speed;
+            }
         }
+        else if(number_player ==2){
+            if (Input.GetKey(KeyCode.D))
+            {
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            //rotate the sprite about the Z axis in the positive direction
-            transform.Rotate(new Vector3(0, 0, 1) * horizontal_speed * Time.deltaTime, Space.World);
+                //rotate the sprite about the Z axis in the positive direction
+                transform.Rotate(new Vector3(0, 0, -1) * horizontal_speed * Time.deltaTime, Space.World);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                //rotate the sprite about the Z axis in the positive direction
+                transform.Rotate(new Vector3(0, 0, 1) * horizontal_speed * Time.deltaTime, Space.World);
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                m_Rigidbody.AddForce(transform.right * 1.0f);
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                //Move the Rigidbody to the left constantly at the speed you define (the red arrow axis in Scene view)
+                m_Rigidbody.velocity = -transform.right * m_Speed;
+            }
+
         }
-
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-
-            m_Rigidbody.AddForce(transform.right * 1.0f);
-
-
-            //Move the Rigidbody to the right constantly at speed you define (the red arrow axis in Scene view)
-            // m_Rigidbody.velocity = transform.right * m_Speed;
-        }
-
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            //Move the Rigidbody to the left constantly at the speed you define (the red arrow axis in Scene view)
-            m_Rigidbody.velocity = -transform.right * m_Speed;
-        }
-        // else{
-        //     m_Rigidbody.velocity = transform.right * 0;
-        // }
+        
     }
 
 
